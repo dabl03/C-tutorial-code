c62e31d8a5270e38d4d429b879b26cb6f65decbf
Daniel Alexander <66857879+dabl03@users.noreply.github.com>
2024-06-16 14:34:46 -0400

    Create prototipo-de-funcion.c

---
+++ cap-3/prototipo-de-funcion.c
@@ -0,0 +1,12 @@
+// https://lechugato.blogspot.com/p/funciones-c.html
+#include <stdio.h>
+void view_head();// Definimos la función, pero no lo que hace.
+
+int main(int argc, char** argc){
+  view_head();
+  return 0;
+}
+void view_head(){
+  puts("Cap 3 - Prototipo de funciones.");
+  puts("Tutorial hecho por dabl03@gmail.com");
+}
