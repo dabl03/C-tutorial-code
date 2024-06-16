c66714209e302611d9ed1138a1d34ac5aa83c5fb
Daniel Alexander <66857879+dabl03@users.noreply.github.com>
2024-06-16 12:39:13 -0400

    Create main.c

---
+++ cap-1/main.c
@@ -0,0 +1,13 @@
+// Código de https://lechugato.blogspot.com/2024/01/tutorial-c.html
+// Esto es un comentario de una línea.
+// Todo en esta línea se ignorará después del //
+#include <stdio.h>
+int main(void){
+    // Mostramos dos mensajes por pantalla
+    puts("Se instalaron las herramientas correctamente.\n");
+    puts("El programa se finalizará cuando presiones enter.");
+    // Pedimos un carácter por la consola.
+    getchar();
+    // Finalizamos el programa.
+    return 0;
+}
